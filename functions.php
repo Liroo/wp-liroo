308f6573597e8b1572c5f550e8f37f2b4faf1994
Liroo <pierre-monge@hotmail.fr>
2019-06-08 02:48:22 +0200

    Fix asset path linked to theme

--- functions.php
+++ functions.php
@@ -28,6 +28,6 @@ add_action( 'wp_before_admin_bar_render', 'remove_comments' );
 	Theme assets
 */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('theme-css', get_template_directory_uri() . "/public/app.css", false, null);
-    wp_enqueue_script('theme-js', get_template_directory_uri() . "/public/app.js", Array(), null, true);
+    wp_enqueue_style('theme-css', get_template_directory_uri() . "/public/css/app.css", false, null);
+    wp_enqueue_script('theme-js', get_template_directory_uri() . "/public/js/app.js", Array(), null, true);
 }, 100);
