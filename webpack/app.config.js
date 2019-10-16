8f645f0119d5f549f517b274dfff6a442b6f4e1c
Liroo <liroo.pierre@gmail.com>
2019-10-16 21:16:53 +0200

    Create assets with hash to handle browser cache

--- webpack/app.config.js
+++ webpack/app.config.js
@@ -19,7 +19,7 @@ module.exports = merge({
 
   outputs: {
     css: {
-      filename: env('FILENAME_CSS', 'css/[name].css')
+      filename: env('FILENAME_CSS', 'css/[name]_[hash].css')
     },
 
     font: {
@@ -31,7 +31,7 @@ module.exports = merge({
     },
 
     javascript: {
-      filename: env('FILENAME_JAVASCRIPT', 'js/[name].js')
+      filename: env('FILENAME_JAVASCRIPT', 'js/[name]_[hash].js')
     },
 
     external: {
